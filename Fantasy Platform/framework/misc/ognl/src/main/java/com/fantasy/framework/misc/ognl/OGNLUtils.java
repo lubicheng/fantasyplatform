feee384a5ea3fc611b15380737340527db842942
lubicheng <lubicheng@outlook.com>
2014-10-16 11:05:53 +0800

    修改了包名，增加了单元测试

---
+++ Fantasy Platform/framework/misc/ognl/src/main/java/com/fantasy/framework/misc/ognl/OGNLUtils.java
@@ -0,0 +1,26 @@
+package com.fantasy.framework.misc.ognl;
+import org.apache.commons.collections.map.LRUMap;
+
+import ognl.Ognl;
+import ognl.OgnlException;
+
+public class OGNLUtils {
+
+	private static LRUMap cache = new LRUMap();
+
+	public static Object getValue(String expr, Object root)
+			throws OgnlException {
+
+		Object exprObject = cache.get("expr");
+
+		if (exprObject == null) {
+
+			exprObject = Ognl.parseExpression(expr);
+			
+			cache.put(expr, exprObject);
+		}
+
+		return Ognl.getValue(exprObject, root); 
+
+	}
+}
