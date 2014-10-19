c96c48289195e3535d34d690115cc156fcdc60c4
lubicheng <lubicheng@outlook.com>
2014-10-19 22:14:43 +0800

    周末的一些修改

---
+++ Fantasy Platform/framework/serivce/core/src/main/java/com/fantasy/framework/service/core/internal/ServiceImpl.java
@@ -0,0 +1,17 @@
+package com.fantasy.framework.service.core.internal;
+
+import com.fantasy.framework.service.core.api.Context;
+import com.fantasy.framework.service.core.api.Service;
+import com.fantasy.framework.service.core.exception.FrameworkException;
+
+public class ServiceImpl implements Service{
+	
+	
+	
+
+	public void execute(Context context) throws FrameworkException {
+		 
+		
+	}
+
+}
