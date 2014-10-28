ef0d34cbfaea3d7a3d6120c1760bb25077249dbf
lubicheng <lubicheng@outlook.com>
2014-10-28 11:32:09 +0800

    调整

--- Fantasy Platform/framework/serivce/core/src/main/java/com/fantasy/framework/service/core/api/ServiceEngine.java
+++ Fantasy Platform/framework/serivce/core/src/main/java/com/fantasy/framework/service/core/api/ServiceEngine.java
@@ -2,7 +2,7 @@ package com.fantasy.framework.service.core.api;
 
 import com.fantasy.framework.service.core.exception.FantasyException;
 
-public interface ServieEngine {
+public interface ServiceEngine {
 
 	ServiceInvoke invoke(Service service, Context context) throws FantasyException;
 	
